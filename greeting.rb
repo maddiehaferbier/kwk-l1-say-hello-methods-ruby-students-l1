701216469626f2a6901bc2c273e452f4d0cb66e6
maddiehaferbier <madisonhaferbier@gmail.com>
2018-06-05 17:07:38 +0000

    automatically backed up by learn

--- greeting.rb
+++ greeting.rb
@@ -1,5 +1,5 @@
 def local_greeting(local_greeting, your_name, my_name, time_of_day)
-puts "#{local_greeting} #{your name}, I am #{my_name}, hows yours #{time_of_day}?"
+puts "#{local_greeting} #{your_name}, I am #{my_name}, hows yours #{time_of_day}?"
 
 end
 
