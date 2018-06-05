723c114d820edefca2937e97bd049c95501d9974
maddiehaferbier <madisonhaferbier@gmail.com>
2018-06-05 16:52:48 +0000

    automatically backed up by learn

---
+++ wake_up.rb
@@ -0,0 +1,11 @@
+def wake_up
+  puts "mom wakes me up "
+  4.times do 
+  puts "finally get up 30 minutes later"
+end
+  puts "gets ready"
+  puts "eats breakfast"
+  puts "leave for school"
+end
+
+wake_up
