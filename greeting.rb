b05da114a69511f5506061cd5ee19b2f2a613387
maddiehaferbier <madisonhaferbier@gmail.com>
2018-06-05 17:08:03 +0000

    automatically backed up by learn

--- greeting.rb
+++ greeting.rb
@@ -3,4 +3,4 @@ puts "#{local_greeting} #{your_name}, I am #{my_name}, hows yours #{time_of_day}
 
 end
 
-local_greeting("hey", "esmy", "maddie", "morning")
+local_greeting("hey", "bay", "maddie", "morning")
